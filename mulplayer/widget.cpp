488d6c1b4945dd4e57dbddaeeeabf0775c29723d
dima <dimabrovkin28538@gmail.com>
2024-04-29 18:52:46 +0500

    init

---
+++ mulplayer/widget.cpp
@@ -0,0 +1,33 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include "QFileDialog"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    m_player = new QMediaPlayer();
+    m_player -> setVolume(70);
+    ui->LabelVolum->setText(QString("Volume: ").append(QString::number(m_player->volume())));
+    ui->horizontalSliderVolume->setValue(m_player->volume());
+}
+
+Widget::~Widget()
+{
+    delete m_player;
+    delete ui;
+}
+
+
+void Widget::on_pushButtonOpen_clicked()
+{
+
+    QString file = QFileDialog::getOpenFileName(this,"Open file",NULL,"Audio files(*mp3 *.flac)");
+    ui->LabelFile->setText(file);
+    m_player->setMedia(QUrl::fromLocalFile(file));
+    m_player->play();
+
+}
+
